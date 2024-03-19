bef8d0b2f11006ef237cc2e596e0385aaf9d73da
jhethia <jhethia@gmail.com>
2024-03-19 09:18:19 +0530

    added valid move check

---
+++ TTTGame/src/main/java/org/practice/Actions/Checks.java
@@ -0,0 +1,30 @@
+package org.practice.Actions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.graalvm.collections.Pair;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Checks {
+
+    public boolean validMove(String[][] board, Pair<Integer,Integer> move){
+        for (int rows = 0; rows < board.length; rows++){
+            for (int cols = 0; cols < board.length; cols++){
+                if(move.getLeft() == rows && move.getRight() == cols){
+                    if(board[rows][cols]!="-"){
+                        System.out.println("!! Cell Is Not Empty. Make A Different Move !!");
+                        return false;
+                    }
+                    else return true;
+                }
+            }
+        }
+        System.out.println("!! Entered Move Is Invalid !!");
+        return false;
+    }
+}
--- TTTGame/src/main/java/org/practice/Entities/GameSetup.java
+++ TTTGame/src/main/java/org/practice/Entities/GameSetup.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.graalvm.collections.Pair;
 
 import java.util.Deque;
+import java.util.Scanner;
 
 @Data
 @AllArgsConstructor
@@ -16,5 +18,13 @@ public class GameSetup {
     private String[][] gameboard;
 
 
-    
+    void startGame(){
+        boolean won = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!won){
+            String[] input = scanner.nextLine().trim().split(",");
+            Pair<Integer,Integer> move = Pair.create(Integer.valueOf(input[0]),Integer.valueOf(input[1]));
+            if()
+        }
+    }
 }
