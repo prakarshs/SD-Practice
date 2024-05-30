57141af99cb7e38f99b5f15e7171ee8c08b6054d
jhethia <jhethia@gmail.com>
2024-05-30 14:45:52 +0530

    making moves

--- TTTP/src/main/java/org/prakarshs/Board.java
+++ TTTP/src/main/java/org/prakarshs/Board.java
@@ -8,15 +8,38 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Board {
-    private Integer row;
-    private Integer col;
+    private Integer size;
 
-    public void initialize(){
-        String[][] board = new String[row][col];
-        for(int i=0; i<this.row; i++){
-            for(int j=0; j<this.col; j++){
+    public String[][] initialize(){
+        String[][] board = new String[size][size];
+        for(int i=0; i<this.size; i++){
+            for(int j=0; j<this.size; j++){
                 board[i][j] = "-";
             }
         }
+        return board;
+    }
+
+    public void print(String[][] board){
+        for(int i=0; i< board.length; i++){
+            for(int j=0; j<board.length; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean valid(String[] move, String[][] gboard) {
+        Integer rowM = Integer.valueOf(move[0]) ;
+        Integer colM = Integer.valueOf(move[1]) ;
+
+        return rowM<=gboard.length && colM<=gboard.length;
+    }
+
+    public boolean canMove(String[] move, String[][] gboard) {
+        Integer rowM = Integer.valueOf(move[0]) - 1;
+        Integer colM = Integer.valueOf(move[1]) - 1;
+
+        return gboard[rowM][colM].equals("-");
     }
 }
---
+++ TTTP/src/main/java/org/prakarshs/GameMachine.java
@@ -0,0 +1,51 @@
+package org.prakarshs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameMachine {
+    private Board board;
+    private Queue<Player> playerList;
+
+    void startGame(){
+        System.out.println("The Game Begins Now!!");
+        String[][] gboard = board.initialize();
+        Scanner sc = new Scanner(System.in);
+        List<String> winners = new ArrayList<>();
+        while(playerList.size() != 1){
+            board.print(gboard);
+            Player player = playerList.poll();
+            System.out.println(player.getPlayerName()+"'s Chance! Enter Your Cordinates!");
+            String[] move = sc.nextLine().trim().split(",");
+            while(!board.valid(move,gboard)){
+                System.out.println("Your Move is Invalid. Try With a Move Within The Playing Board.");
+                move = sc.nextLine().trim().split(",");
+            }
+            while(!board.canMove(move,gboard)){
+                System.out.println("You Cant Move An Already Checked Cell. Try With a Move Amongst The Empty Cells.");
+                move = sc.nextLine().trim().split(",");
+            }
+            if(board.hasWon(move,gboard)){
+                gboard = board.makeMove(move,gboard);
+                System.out.println(player.getPlayerName()+" Has Won With The Piece "+player.getPlayerType()+" !!");
+                winners.add(player.getPlayerName());
+                board.print(gboard);
+            }
+            else{
+                gboard = board.makeMove(move,gboard);
+                board.print(gboard);
+                playerList.add(player);
+            }
+
+        }
+    }
+}
